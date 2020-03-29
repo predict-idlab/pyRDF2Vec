d44118fbd5c91e35b75b2708388e8b8b40ecc587
Gilles Vandewiele <givdwiel.vandewiele@ugent.be>
2020-03-29 16:32:03 +0200

    updated example and setup

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(name='pyRDF2Vec',
-      version='0.0.3',
+      version='0.0.5',
       description='A python implementation of RDF2Vec',
       authors='Gilles Vandewiele, Bram Steenwinckel, Michael Weyns',
       author_email='gilles.vandewiele@ugent.be',
       url='https://github.com/IBCNServices/pyRDF2Vec',
       packages=['rdf2vec', 'rdf2vec/walkers'],
       install_requires=['gensim', 'matplotlib', 'networkx', 'numpy', 
-          'pandas', 'rdflib', 'scikit_learn', 'tqdm'
+          'pandas', 'rdflib', 'scikit_learn', 'tqdm', 'python-louvain'
       ]
 )
