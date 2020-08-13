86d68030b240e01814bc1be0d306a057abe7f373
Terencio Agozzino <terencio.agozzino@gmail.com>
2020-08-13 06:54:11 +0200

    refactor: add a better display for setup

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
 from setuptools import setup
 
-setup(name='pyRDF2Vec',
-      version='0.0.5',
-      description='A python implementation of RDF2Vec',
-      authors='Gilles Vandewiele, Bram Steenwinckel, Michael Weyns',
-      author_email='gilles.vandewiele@ugent.be',
-      url='https://github.com/IBCNServices/pyRDF2Vec',
-      packages=['rdf2vec', 'rdf2vec/walkers'],
-      install_requires=['gensim', 'matplotlib', 'networkx', 'numpy', 
-          'pandas', 'rdflib', 'scikit_learn', 'tqdm', 'python-louvain'
-      ]
+setup(
+    name="pyRDF2Vec",
+    version="0.0.5",
+    description="A python implementation of RDF2Vec",
+    authors="Gilles Vandewiele, Bram Steenwinckel, Michael Weyns",
+    author_email="gilles.vandewiele@ugent.be",
+    url="https://github.com/IBCNServices/pyRDF2Vec",
+    packages=["rdf2vec", "rdf2vec/walkers"],
+    install_requires=[
+        "gensim",
+        "matplotlib",
+        "networkx",
+        "numpy",
+        "pandas",
+        "rdflib",
+        "scikit_learn",
+        "tqdm",
+        "python-louvain",
+    ],
 )
