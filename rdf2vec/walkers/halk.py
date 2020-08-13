5d097248a4762a973494fba1f559ff3f4bd6507b
Terencio Agozzino <terencio.agozzino@gmail.com>
2020-08-13 06:38:22 +0200

    refactor: fix imports for halk

--- rdf2vec/walkers/halk.py
+++ rdf2vec/walkers/halk.py
@@ -1,8 +1,11 @@
 from collections import defaultdict
-from rdf2vec.walkers import RandomWalker
-from rdf2vec.graph import Vertex
-import numpy as np
 from hashlib import md5
+
+import numpy as np
+
+from rdf2vec.graph import Vertex
+from rdf2vec.walkers import RandomWalker
+
 
 class HalkWalker(RandomWalker):
     def __init__(self, depth, walks_per_graph, 
