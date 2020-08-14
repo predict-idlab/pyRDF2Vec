a1f6acfa641f99bab11625f0a5c93c44d8efd37f
Terencio Agozzino <terencio.agozzino@gmail.com>
2020-08-14 09:52:37 +0200

    style: add the WildcardWalker's documentation

--- rdf2vec/walkers/wildcard.py
+++ rdf2vec/walkers/wildcard.py
@@ -5,11 +5,35 @@ import itertools
 from hashlib import md5
 
 class WildcardWalker(RandomWalker):
+    """Defines the wild card of the walking strategy.
+
+    Attributes:
+        depth (int): The depth per entity.
+        walks_per_graph (float): The maximum number of walks per entity.
+    """
+
     def __init__(self, depth, walks_per_graph, wildcards=[1]):
         super(WildcardWalker, self).__init__(depth, walks_per_graph)
         self.wildcards = wildcards
 
     def extract(self, graph, instances):
+        """Extracts a knowledge graph and transform it into a 2D vector, based
+        on provided instances.
+
+        Note:
+            You can create a `graph.KnowledgeGraph` object from an
+            `rdflib.Graph` object by using a converter method.
+
+        Args:
+            graph (graph.KnowledgeGraph): The knowledge graph.
+                The graph from which the neighborhoods are extracted for the
+                provided instances.
+            instances (array-like): The instances to extract the knowledge graph.
+
+        Returns:
+            list: The 2D vector corresponding to the knowledge graph.
+
+        """
         canonical_walks = set()
         for instance in instances:
             walks = self.extract_random_walks(graph, Vertex(str(instance)))
