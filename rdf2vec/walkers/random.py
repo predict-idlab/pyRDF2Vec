aa2adb7239236b3562feed1cf1dbdc769f008214
Terencio Agozzino <terencio.agozzino@gmail.com>
2020-08-14 10:08:27 +0200

    style: add the RandomWalker's documentation

--- rdf2vec/walkers/random.py
+++ rdf2vec/walkers/random.py
@@ -5,11 +5,34 @@ from hashlib import md5
 
 
 class RandomWalker(Walker):
+    """Defines the random walker of the walking strategy.
+
+    Attributes:
+        depth (int): The depth per entity.
+        walks_per_graph (float): The maximum number of walks per entity.
+
+    """
+
     def __init__(self, depth, walks_per_graph):
         super(RandomWalker, self).__init__(depth, walks_per_graph)
 
     def extract_random_walks(self, graph, root):
-        """Extract random walks of depth - 1 hops rooted in root."""
+        """Extracts random walks of depth - 1 hops rooted in root.
+
+        Note:
+            You can create a `graph.KnowledgeGraph` object from an
+            `rdflib.Graph` object by using a converter method.
+
+        Args:
+            graph (graph.KnowledgeGraph): The knowledge graph.
+                The graph from which the neighborhoods are extracted for the
+                provided instances.
+            root (Vertex): The root.
+
+        Returns:
+            numpy.array: The array of the walks.
+
+        """
         # Initialize one walk of length 1 (the root)
         walks = {(root,)}
 
@@ -40,6 +63,23 @@ class RandomWalker(Walker):
         return list(walks)
 
     def extract(self, graph, instances):
+        """Extracts a knowledge graph and transform it into a 2D vector, based
+        on provided instances.
+
+        Note:
+            You can create a `graph.KnowledgeGraph` object from an
+            `rdflib.Graph` object by using a converter method.
+
+        Args:
+            graph (graph.KnowledgeGraph): The knowledge graph.
+                The graph from which the neighborhoods are extracted for the
+                provided instances.
+            instances (array-like): The instances to extract the knowledge graph.
+
+        Returns:
+            list: The 2D vector corresponding to the knowledge graph.
+
+        """
         canonical_walks = set()
         for instance in instances:
             walks = self.extract_random_walks(graph, Vertex(str(instance)))
