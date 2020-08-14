bf13c06db46a8a11c674a7ed26f14afc9210ad73
Terencio Agozzino <terencio.agozzino@gmail.com>
2020-08-14 10:09:32 +0200

    style: add the HalkWalker's documentation

--- rdf2vec/walkers/halk.py
+++ rdf2vec/walkers/halk.py
@@ -5,13 +5,37 @@ import numpy as np
 from hashlib import md5
 
 class HalkWalker(RandomWalker):
-    def __init__(self, depth, walks_per_graph, 
-                 freq_thresholds=[0.001]):
+    """Defines the halk walker of the walking strategy.
+
+    Attributes:
+        depth (int): The depth per entity.
+        walks_per_graph (float): The maximum number of walks per entity.
+
+    """
+
+    def __init__(self, depth, walks_per_graph, freq_thresholds=[0.001]):
         super(HalkWalker, self).__init__(depth, walks_per_graph)
         self.freq_thresholds = freq_thresholds
         # self.lb_freq_threshold = lb_freq_threshold
 
     def extract(self, graph, instances):
+        """Extracts a knowledge graph and transform it into a 2D vector, based
+        on provided instances.
+
+        Note:
+            You can create a `graph.KnowledgeGraph` object from an
+            `rdflib.Graph` object by using a converter method.
+
+        Args:
+            graph (graph.KnowledgeGraph): The knowledge graph.
+                The graph from which the neighborhoods are extracted for the
+                provided instances.
+            instances (array-like): The instances to extract the knowledge graph.
+
+        Returns:
+            list: The 2D vector corresponding to the knowledge graph.
+
+        """
         canonical_walks = set()
         all_walks=[]
         for instance in instances:
