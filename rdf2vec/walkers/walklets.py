3c41062ac09e83e9bc9f85ec58b7432de3cf978e
Terencio Agozzino <terencio.agozzino@gmail.com>
2020-08-14 09:58:27 +0200

    style: fix the walkets's documentation

--- rdf2vec/walkers/walklets.py
+++ rdf2vec/walkers/walklets.py
@@ -4,10 +4,35 @@ import numpy as np
 from hashlib import md5
 
 class WalkletWalker(RandomWalker):
+    """Defines the walklet walker of the walking strategy.
+
+    Attributes:
+        depth (int): The depth per entity.
+        walks_per_graph (float): The maximum number of walks per entity.
+
+    """
+
     def __init__(self, depth, walks_per_graph):
         super(WalkletWalker, self).__init__(depth, walks_per_graph)
 
     def extract(self, graph, instances):
+         """Extracts a knowledge graph and transform it into a 2D vector, based
+        on provided instances.
+
+        Note:
+            You can create a `graph.KnowledgeGraph` object from an
+            `rdflib.Graph` object by using a converter method.
+
+        Args:
+            graph (graph.KnowledgeGraph): The knowledge graph.
+                The graph from which the neighborhoods are extracted for the
+                provided instances.
+            instances (array-like): The instances to extract the knowledge graph.
+
+        Returns:
+            list: The 2D vector corresponding to the knowledge graph.
+
+        """
         canonical_walks = set()
         for instance in instances:
             walks = self.extract_random_walks(graph, Vertex(str(instance)))
