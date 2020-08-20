902e6e2037866d22f185f3bc8b5e184bd694fca0
Terencio Agozzino <terencio.agozzino@gmail.com>
2020-08-20 11:16:56 +0200

    feat: add more tests for Vertex

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,5 +1,28 @@
 from rdf2vec.graph import Vertex
 
+s_v = Vertex("s")
+o_v = Vertex("o")
+p_v = Vertex("p", predicate=True, _from=s_v, _to=o_v)
 
-def test_eq_vertex():
-    assert Vertex("s") == Vertex("s")
+
+class TestVertex:
+    def test_eq(self):
+        assert s_v == s_v
+
+    def test_hash_without_predicate(self):
+        assert hash(s_v) == hash("s")
+
+    def test_hash_with_predicate(self):
+        assert hash(p_v) == hash((p_v.id, s_v, o_v, "p"))
+
+    def test_id_init(self):
+        assert s_v.id == 0
+
+    def test_id_incremental(self):
+        assert o_v.id == 1
+
+    def test_lt(self):
+        assert o_v < s_v
+
+    def test_neq(self):
+        assert s_v != o_v
