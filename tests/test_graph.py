6e8690d583f47ccdff13a15a65b8249f3b610541
Terencio Agozzino <terencio.agozzino@gmail.com>
2020-08-20 11:22:13 +0200

    refactor: sorts tests

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -9,17 +9,17 @@ class TestVertex:
     def test_eq(self):
         assert a == a
 
-    def test_hash_without_predicate(self):
-        assert hash(a) == hash("a")
-
     def test_hash_with_predicate(self):
         assert hash(c) == hash((c.id, a, b, "c"))
 
-    def test_id_init(self):
-        assert a.id == 0
+    def test_hash_without_predicate(self):
+        assert hash(a) == hash("a")
 
     def test_id_incremental(self):
         assert b.id == 1
+
+    def test_id_init(self):
+        assert a.id == 0
 
     def test_lt(self):
         assert a < b
