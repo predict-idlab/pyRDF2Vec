d8ed2054e4d323b7b64e35234ff45acef6b54a0b
Terencio Agozzino <terencio.agozzino@gmail.com>
2020-08-21 11:46:33 +0200

    style: fix coding styles for example

--- example.py
+++ example.py
@@ -2,19 +2,19 @@ import os
 import random
 import warnings
 
-import numpy as np
-
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import rdflib
-from rdf2vec import RDF2VecTransformer
-from rdf2vec.converters import rdflib_to_kg
-from rdf2vec.walkers import RandomWalker
 from sklearn.manifold import TSNE
 from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.svm import SVC
 
-warnings.filterwarnings('ignore')
+from rdf2vec import RDF2VecTransformer
+from rdf2vec.converters import rdflib_to_kg
+from rdf2vec.walkers import RandomWalker
+
+warnings.filterwarnings("ignore")
 
 np.random.seed(42)
 random.seed(42)
@@ -24,14 +24,14 @@ random.seed(42)
 #########################################################################
 
 # Load our train & test instances and labels
-test_data = pd.read_csv('sample/MUTAG_test.tsv', sep='\t')
-train_data = pd.read_csv('sample/MUTAG_train.tsv', sep='\t')
+test_data = pd.read_csv("sample/MUTAG_test.tsv", sep="\t")
+train_data = pd.read_csv("sample/MUTAG_train.tsv", sep="\t")
 
-train_entities = [rdflib.URIRef(x) for x in train_data['bond']]
-train_labels = train_data['label_mutagenic']
+train_entities = [rdflib.URIRef(x) for x in train_data["bond"]]
+train_labels = train_data["label_mutagenic"]
 
-test_entities = [rdflib.URIRef(x) for x in test_data['bond']]
-test_labels = test_data['label_mutagenic']
+test_entities = [rdflib.URIRef(x) for x in test_data["bond"]]
+test_labels = test_data["label_mutagenic"]
 
 all_entities = train_entities + test_entities
 all_labels = list(train_labels) + list(test_labels)
@@ -41,22 +41,22 @@ all_labels = list(train_labels) + list(test_labels)
 label_predicates = ["http://dl-learner.org/carcinogenesis#isMutagenic"]
 
 # Convert the rdflib to our KnowledgeGraph object
-kg = rdflib_to_kg('sample/mutag.owl', label_predicates=label_predicates)
+kg = rdflib_to_kg("sample/mutag.owl", label_predicates=label_predicates)
 
 #########################################################################
 #                          CREATING EMBEDDINGS                          #
 #########################################################################
 
 # We'll all possible walks of depth 2
-random_walker = RandomWalker(4, float('inf'))
+random_walker = RandomWalker(4, float("inf"))
 
 # Create embeddings with random walks
 transformer = RDF2VecTransformer(walkers=[random_walker], sg=1)
 walk_embeddings = transformer.fit_transform(kg, all_entities)
 
 # Split into train and test embeddings
-train_embeddings = walk_embeddings[:len(train_entities)]
-test_embeddings = walk_embeddings[len(train_entities):]
+train_embeddings = walk_embeddings[: len(train_entities)]
+test_embeddings = walk_embeddings[len(train_entities) :]
 
 #########################################################################
 #                              FIT CLASSIFIER                           #
@@ -66,7 +66,7 @@ test_embeddings = walk_embeddings[len(train_entities):]
 clf = SVC(random_state=42)
 clf.fit(train_embeddings, train_labels)
 
-print(end='Support Vector Machine: Accuracy = ')
+print(end="Support Vector Machine: Accuracy = ")
 print(accuracy_score(test_labels, clf.predict(test_embeddings)))
 print(confusion_matrix(test_labels, clf.predict(test_embeddings)))
 
@@ -79,7 +79,7 @@ walk_tsne = TSNE(random_state=42)
 X_walk_tsne = walk_tsne.fit_transform(walk_embeddings)
 
 # Define a color map
-colors = ['r', 'g']
+colors = ["r", "g"]
 color_map = {}
 for i, label in enumerate(set(all_labels)):
     color_map[label] = colors[i]
@@ -87,39 +87,41 @@ for i, label in enumerate(set(all_labels)):
 # Plot the train embeddings
 plt.figure(figsize=(10, 4))
 plt.scatter(
-    X_walk_tsne[:len(train_entities), 0],
-    X_walk_tsne[:len(train_entities), 1],
-    edgecolors=[color_map[i] for i in all_labels[:len(train_entities)]],
-    facecolors=[color_map[i] for i in all_labels[:len(train_entities)]],
+    X_walk_tsne[: len(train_entities), 0],
+    X_walk_tsne[: len(train_entities), 1],
+    edgecolors=[color_map[i] for i in all_labels[: len(train_entities)]],
+    facecolors=[color_map[i] for i in all_labels[: len(train_entities)]],
 )
 
 # Plot the test embeddings
 plt.scatter(
-    X_walk_tsne[len(train_entities):, 0],
-    X_walk_tsne[len(train_entities):, 1],
-    edgecolors=[color_map[i] for i in all_labels[len(train_entities):]],
-    facecolors='none'
+    X_walk_tsne[len(train_entities) :, 0],
+    X_walk_tsne[len(train_entities) :, 1],
+    edgecolors=[color_map[i] for i in all_labels[len(train_entities) :]],
+    facecolors="none",
 )
 
 # Annotate a few points
 for i, ix in enumerate([25, 35]):
     plt.annotate(
-        all_entities[ix].split('/')[-1],
-        xy=(X_walk_tsne[ix, 0], X_walk_tsne[ix, 1]), xycoords='data',
+        all_entities[ix].split("/")[-1],
+        xy=(X_walk_tsne[ix, 0], X_walk_tsne[ix, 1]),
+        xycoords="data",
         xytext=(0.1 * i, 0.05 + 0.1 * i),
-        fontsize=8, textcoords='axes fraction',
-        arrowprops=dict(arrowstyle="->", facecolor='black')
+        fontsize=8,
+        textcoords="axes fraction",
+        arrowprops=dict(arrowstyle="->", facecolor="black"),
     )
 
 # Create a legend
-plt.scatter([], [], edgecolors='r', facecolors='r', label='train -')
-plt.scatter([], [], edgecolors='g', facecolors='g', label='train +')
-plt.scatter([], [], edgecolors='r', facecolors='none', label='test -')
-plt.scatter([], [], edgecolors='g', facecolors='none', label='test +')
-plt.legend(loc='upper right', ncol=2)
+plt.scatter([], [], edgecolors="r", facecolors="r", label="train -")
+plt.scatter([], [], edgecolors="g", facecolors="g", label="train +")
+plt.scatter([], [], edgecolors="r", facecolors="none", label="test -")
+plt.scatter([], [], edgecolors="g", facecolors="none", label="test +")
+plt.legend(loc="upper right", ncol=2)
 
 # Show & save the figure
-plt.title('pyRDF2Vec', fontsize=32)
-plt.axis('off')
-plt.savefig('embeddings.png')
+plt.title("pyRDF2Vec", fontsize=32)
+plt.axis("off")
+plt.savefig("embeddings.png")
 plt.show()
