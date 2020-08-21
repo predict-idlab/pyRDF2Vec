96d5783dd1562dfa0007139a7c8e9c18a47d9226
Terencio Agozzino <terencio.agozzino@gmail.com>
2020-08-21 11:43:42 +0200

    refactor: sort functions in Walker

--- rdf2vec/walkers/walker.py
+++ rdf2vec/walkers/walker.py
@@ -11,6 +11,24 @@ class Walker:
         self.depth = depth
         self.walks_per_graph = walks_per_graph
 
+    def extract(self, graph, instances):
+        """Extracts walks rooted at the provided instances which are then each
+        transformed into a numerical representation.
+
+        Args:
+            graph (graph.KnowledgeGraph): The knowledge graph.
+                The graph from which the neighborhoods are extracted for the
+                provided instances.
+            instances (array-like): The instances to extract the knowledge graph.
+
+        Returns:
+            list: The 2D matrix with its:
+                number of rows equal to the number of provided instances;
+                number of column equal to the embedding size.
+
+        """
+        raise NotImplementedError("This must be implemented!")
+
     def print_walks(self, graph, instances, file_name):
         """Prints the walks of a knowledge graph.
 
@@ -35,28 +53,9 @@ class Walker:
 
                 if i < len(walk) - 1:
                     s += "--> "
-
             walk_strs.append(s)
 
         with open(file_name, "w+") as myfile:
             for s in walk_strs:
                 myfile.write(s)
                 myfile.write("\n\n")
-
-    def extract(self, graph, instances):
-        """Extracts walks rooted at the provided instances which are then each
-        transformed into a numerical representation.
-
-        Args:
-            graph (graph.KnowledgeGraph): The knowledge graph.
-                The graph from which the neighborhoods are extracted for the
-                provided instances.
-            instances (array-like): The instances to extract the knowledge graph.
-
-        Returns:
-            list: The 2D matrix with its:
-                number of rows equal to the number of provided instances;
-                number of column equal to the embedding size.
-
-        """
-        raise NotImplementedError("This must be implemented!")
