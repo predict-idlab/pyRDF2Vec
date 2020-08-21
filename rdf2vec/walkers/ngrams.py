01d62076004bb24c25d80a419845b976608a7166
Terencio Agozzino <terencio.agozzino@gmail.com>
2020-08-21 11:39:23 +0200

    refactor: give an explicit name for grams

--- rdf2vec/walkers/ngrams.py
+++ rdf2vec/walkers/ngrams.py
@@ -13,18 +13,18 @@ class NGramWalker(RandomWalker):
     Attributes:
         depth (int): The depth per entity.
         walks_per_graph (float): The maximum number of walks per entity.
-        n (int): The number of grams.
+        grams (int): The number of grams.
             Defaults to 3.
         wildcards (list): the wild cards.
             Defaults to None.
 
     """
 
-    def __init__(self, depth, walks_per_graph, n=3, wildcards=None):
+    def __init__(self, depth, walks_per_graph, grams=3, wildcards=None):
         super().__init__(depth, walks_per_graph)
-        self.n = n
-        self.wildcards = wildcards
+        self.grams = grams
         self.n_gram_map = {}
+        self.wildcards = wildcards
 
     def _take_n_grams(self, walk):
         """Takes the N-Grams.
@@ -38,16 +38,16 @@ class NGramWalker(RandomWalker):
         """
         n_gram_walk = []
         for i, hop in enumerate(walk):
-            if i == 0 or i % 2 == 1 or i < self.n:
+            if i == 0 or i % 2 == 1 or i < self.grams:
                 n_gram_walk.append(hop.name)
             else:
                 n_gram = tuple(
-                    walk[j].name for j in range(max(0, i - (self.n - 1)), i + 1)
+                    walk[j].name
+                    for j in range(max(0, i - (self.grams - 1)), i + 1)
                 )
                 if n_gram not in self.n_gram_map:
                     self.n_gram_map[n_gram] = str(len(self.n_gram_map))
-                n_gram_walk.append(self.n_gram_map[n_gram])
-
+                n_gram_walk.append(self._gram_map[n_gram])
         return n_gram_walk
 
     def extract(self, graph, instances):
@@ -83,5 +83,7 @@ class NGramWalker(RandomWalker):
                         new_walk = list(walk).copy()
                         for ix in idx:
                             new_walk[ix] = Vertex("*")
-                        canonical_walks.add(tuple(self._take_n_grams(new_walk)))
+                        canonical_walks.add(
+                            tuple(self._take_n_grams(new_walk))
+                        )
         return canonical_walks
