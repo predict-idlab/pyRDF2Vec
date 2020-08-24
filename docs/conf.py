beb00b3c4621f17dbb6d173689f5d423195f2d82
Terencio Agozzino <terencio.agozzino@gmail.com>
2020-08-24 11:59:50 +0200

    style: update the Sphinx's documentation

--- docs/conf.py
+++ docs/conf.py
@@ -4,48 +4,16 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-
-# -- Project information -----------------------------------------------------
-
-project = "pyRDF2Vec"
-copyright = "2020, Gilles Vandewiele, Bram Steenwinckel, Michael Weyns"
 author = "Gilles Vandewiele, Bram Steenwinckel, Michael Weyns"
+copyright = "2020, Gilles Vandewiele, Bram Steenwinckel, Michael Weyns"
+project = "pyRDF2Vec"
+source_suffix = [".rst", ".md"]
+version = "0.5"
 
-
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = ["sphinxcontrib.napoleon"]
-
-# Add any paths that contain templates here, relative to this directory.
+exclude_patterns = ["_build"]
+extensions = ["sphinx_rtd_theme", "sphinxcontrib.napoleon"]
+pygments_style = "sphinx"
 templates_path = ["_templates"]
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = "alabaster"
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
