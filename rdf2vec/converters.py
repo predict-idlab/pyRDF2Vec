bcec2cce4e07c567f23503a6e529944ba1e7d83c
Terencio Agozzino <terencio.agozzino@gmail.com>
2020-08-25 11:42:16 +0200

    fix: endpoint_to_kg with SPARQL endpoint

--- rdf2vec/converters.py
+++ rdf2vec/converters.py
@@ -72,18 +72,18 @@ def endpoint_to_kg(
 
     """
     session = requests.Session()
-    session.mount("http://", adapter)
     adapter = requests.adapters.HTTPAdapter(
         pool_connections=100, pool_maxsize=100
     )
+    session.mount("http://", adapter)
 
     query = urllib.parse.quote("SELECT ?s ?p ?o WHERE { ?s ?p ?o }")
     try:
-        qres = r.json()["results"]["bindings"]
         r = session.get(
             endpoint_url + query,
             headers={"Accept": "application/sparql-results+json"},
         )
+        qres = r.json()["results"]["bindings"]
     except Exception as e:
         print(e)
         print("could not query result")
