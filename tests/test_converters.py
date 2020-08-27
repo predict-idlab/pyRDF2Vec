a7860815ce50b239a50f7fbf4cb625d7d3b427f6
Terencio Agozzino <terencio.agozzino@gmail.com>
2020-08-27 13:15:17 +0200

    feat: add a test start for converters

---
+++ tests/test_converters.py
@@ -0,0 +1,24 @@
+import rdflib
+
+from rdf2vec.converters import create_kg, endpoint_to_kg, rdflib_to_kg
+from rdf2vec.graph import KnowledgeGraph
+
+label_predicates = ["http://dl-learner.org/carcinogenesis#isMutagenic"]
+
+
+def test_create_kg():
+    kg = create_kg(rdflib.Graph(), label_predicates=label_predicates)
+    assert type(kg) == KnowledgeGraph
+
+
+def test_endpoint_to_kg():
+    kg = endpoint_to_kg(
+        "http://localhost:5820/db/query?query=",
+        label_predicates=label_predicates,
+    )
+    assert type(kg) == KnowledgeGraph
+
+
+def test_rdflib_to_kg():
+    kg = rdflib_to_kg("samples/mutag.owl", label_predicates=label_predicates)
+    assert type(kg) == KnowledgeGraph
