c788c83b12630246caa57e132582b5844f8633a5
Terencio Agozzino <terencio.agozzino@gmail.com>
2020-08-28 14:10:29 +0200

    feat: add a starter test for RandomWalker

---
+++ tests/test_walkers.py
@@ -0,0 +1,33 @@
+import random
+
+import rdflib
+
+from rdf2vec.converters import rdflib_to_kg
+from rdf2vec.graph import Vertex
+from rdf2vec.walkers import RandomWalker
+
+LABEL_PREDICATE = "http://dl-learner.org/carcinogenesis#isMutagenic"
+KG = rdflib_to_kg("samples/mutag.owl", label_predicates=[LABEL_PREDICATE])
+
+
+def generate_entities():
+    return [
+        rdflib.URIRef(
+            f"{LABEL_PREDICATE.split('#')[0] + '#'}{random.randint(0, 335)}"
+        )
+        for _ in range(random.randint(0, 200))
+    ]
+
+
+class TestRandomWalker:
+    def test_extract_random_walks(self):
+        walks = RandomWalker(4, float("inf")).extract_random_walks(
+            KG, Vertex(str(generate_entities()))
+        )
+        assert type(walks) == list
+
+    def test_extract(self):
+        canonical_walks = RandomWalker(4, float("inf")).extract(
+            KG, generate_entities()
+        )
+        assert type(canonical_walks) == set
