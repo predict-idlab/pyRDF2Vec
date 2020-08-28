9fbdcdd3a73109ed144ed86d4d9d58e285a05ec5
Terencio Agozzino <terencio.agozzino@gmail.com>
2020-08-28 18:09:09 +0200

    feat: add start to test KnowledgeGraph

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,3 +1,4 @@
+from rdf2vec.converters import rdflib_to_kg
 from rdf2vec.graph import Vertex
 
 a = Vertex("a")
@@ -20,3 +21,13 @@ class TestVertex:
 
     def test_neq(self):
         assert a != b
+
+
+LABEL_PREDICATE = "http://dl-learner.org/carcinogenesis#isMutagenic"
+KG = rdflib_to_kg("samples/mutag.owl", label_predicates=[LABEL_PREDICATE])
+
+
+class TestKnowledgeGraph:
+    def test_visualise(self):
+        KG.visualise()
+        assert True
