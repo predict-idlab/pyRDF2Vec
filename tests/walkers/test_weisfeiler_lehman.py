012f2d289d1a4f6ff1d8d5bfe334ec8cce9aa8c3
Terencio Agozzino <terencio.agozzino@gmail.com>
2020-09-09 14:28:30 +0200

    style: improve the errors display for Weisfeiler

--- tests/walkers/test_weisfeiler_lehman.py
+++ tests/walkers/test_weisfeiler_lehman.py
@@ -4,6 +4,9 @@ import rdflib
 
 from pyrdf2vec.converters import rdflib_to_kg
 
+# from pyrdf2vec.graph import Vertex
+# from pyrdf2vec.walkers import WeisfeilerLehmanWalker
+
 LABEL_PREDICATE = "http://dl-learner.org/carcinogenesis#isMutagenic"
 KG = rdflib_to_kg("samples/mutag.owl", label_predicates=[LABEL_PREDICATE])
 
@@ -26,4 +29,23 @@ class TestWeisfeilerLehmanWalker:
         #     KG, generate_entities()
         # )
         # assert type(canonical_walks) == set
+        pass
+
+    def test_create_label(self):
+        # AttributeError: 'WeisfeilerLehmanWalker' object has no attribute
+        # '_label_map'
+        #
+        # WeisfeilerLehmanWalker(4, float("inf"))._create_label(
+        #     KG, Vertex("a"), 0
+        # )
+        # assert True
+        pass
+
+    def test_weisfeiler_lehman(self):
+        # KeyError:
+        # neighbor_names = [
+        # self._label_map[x][n - 1] for x in graph.get_inv_neighbors(vertex)
+        #
+        # WeisfeilerLehmanWalker(4, float("inf"))._weisfeiler_lehman(KG)
+        # assert True
         pass
