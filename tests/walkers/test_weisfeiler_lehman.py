b586aa936d4c95fadc4f98b7ccf7cd1f9652c878
Terencio Agozzino <terencio.agozzino@gmail.com>
2020-09-13 15:56:12 +0200

    fix: tests with WeisfeilerLehmanWalker

--- tests/walkers/test_weisfeiler_lehman.py
+++ tests/walkers/test_weisfeiler_lehman.py
@@ -3,7 +3,7 @@ from typing import List
 
 import rdflib
 
-from pyrdf2vec.graphs import KnowledgeGraph, Vertex
+from pyrdf2vec.graphs import KnowledgeGraph
 from pyrdf2vec.samplers import UniformSampler
 from pyrdf2vec.walkers import WeisfeilerLehmanWalker
 
@@ -28,14 +28,6 @@ class TestWeisfeilerLehmanWalker:
             2, 5, UniformSampler()
         ).extract(KG, generate_entities())
         assert type(canonical_walks) == set
-        pass
-
-    def test_create_label(self):
-        WeisfeilerLehmanWalker(2, 5, UniformSampler())._create_label(
-            KG, Vertex("a"), 0
-        )
-        assert True
-        pass
 
     def test_weisfeiler_lehman(self):
         WeisfeilerLehmanWalker(2, 5, UniformSampler())._weisfeiler_lehman(KG)
