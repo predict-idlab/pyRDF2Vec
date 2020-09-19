63f6c490fb4415a24c6e829de2d8c79257ee84a4
Terencio Agozzino <terencio.agozzino@gmail.com>
2020-09-19 09:33:42 +0200

    refactor: test_walkers

--- tests/test_walkers.py
+++ tests/test_walkers.py
@@ -2,6 +2,7 @@ import inspect
 import pkgutil
 import random
 from operator import itemgetter
+from typing import List, Sequence, Tuple, TypeVar
 
 import numpy as np
 import pandas as pd
@@ -17,52 +18,69 @@ np.random.seed(42)
 random.seed(42)
 
 LABEL_PREDICATE = "http://dl-learner.org/carcinogenesis#isMutagenic"
-kg = KG("samples/mutag/mutag.owl", label_predicates=[LABEL_PREDICATE])
+KNOWLEDGE_GRAPH = KG(
+    "samples/mutag/mutag.owl", label_predicates=[LABEL_PREDICATE]
+)
 LEAKY_KG = KG("samples/mutag/mutag.owl", label_predicates=[])
-train_df = pd.read_csv("samples/mutag/train.tsv", sep="\t", header=0)
-entities = [rdflib.URIRef(x) for x in train_df["bond"]]
-entities_subset = entities[:5]
+TRAIN_DF = pd.read_csv("samples/mutag/train.tsv", sep="\t", header=0)
+ENTITIES = [rdflib.URIRef(x) for x in TRAIN_DF["bond"]]
+ENTITIES_SUBSET = ENTITIES[:5]
+
+T = TypeVar("T")
 
 
-def is_abstract(c):
-    if not (hasattr(c, "__abstractmethods__")):
-        return False
-    if not len(c.__abstractmethods__):
-        return False
-    return True
+def _get_classes() -> List[T]:
+    """Gets the classes from a package.
 
+    Returns:
+        The classes from a package.
 
-def _get_all_classes():
-    # Walk through all the packages from our base_path and
-    # add all the classes to a list
-    all_classes = []
+    """
+    classes = []
     base_path = pyrdf2vec.__path__
     for _, name, _ in pkgutil.walk_packages(
         path=base_path, prefix="pyrdf2vec."
     ):
         module = __import__(name, fromlist="dummy")
-        all_classes.extend(inspect.getmembers(module, inspect.isclass))
-    return all_classes
+        classes.extend(inspect.getmembers(module, inspect.isclass))
+    return classes
 
 
-def _get_walkers():
-    all_classes = _get_all_classes()
-    # Filter out those that are not a subclass of `sklearn.BaseEstimator`
-    all_classes = [c for c in set(all_classes) if issubclass(c[1], Walker)]
-    # get rid of abstract base classes
-    all_classes = filter(lambda c: not is_abstract(c[1]), all_classes)
-    return sorted(set(all_classes), key=itemgetter(0))
+def _get_walkers() -> List[Tuple[str, T]]:
+    """Gets the classes that are not a subclass of `sklearn.BaseEstimator` and
+    that are not an abstract class.
+
+    Returns:
+        The classes.
+
+    """
+    classes = [c for c in set(_get_classes()) if issubclass(c[1], Walker)]
+    classes = filter(lambda c: not is_abstract(c[1]), classes)
+    return sorted(set(classes), key=itemgetter(0))
 
 
 def check_walker(Walker):
     canonical_walks = Walker(2, 5, UniformSampler()).extract(
-        kg, entities_subset
+        KNOWLEDGE_GRAPH, ENTITIES_SUBSET
     )
     assert type(canonical_walks) == set
 
 
+def is_abstract(c: Sequence[T]) -> bool:
+    """Tells whether a class is abstract or not.
+
+    Args:
+        c: The class has to determine if it is abstract or not.
+
+    Returns:
+        True if abstract class, False otherwise.
+
+    """
+    return hasattr(c, "__abstractmethods__") and len(c.__abstractmethods__)
+
+
 @pytest.mark.parametrize("name, Walker", _get_walkers())
-def test_all_walkers(name, Walker):
-    """Test all the estimators in tslearn."""
+def test_walkers(name: str, Walker: T):
+    """Tests the estimators in tslearn."""
     print(f"Testing {name}")
     check_walker(Walker)
