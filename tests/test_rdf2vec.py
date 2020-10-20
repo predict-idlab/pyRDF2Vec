633f4ed1aa8d77eb516f954e0ff18d5ec9f8314e
Terencio Agozzino <terencio.agozzino@gmail.com>
2020-10-20 19:31:54 +0200

    style: avoid duplications for test_rdf2vec

--- tests/test_rdf2vec.py
+++ tests/test_rdf2vec.py
@@ -10,8 +10,6 @@ from sklearn.exceptions import NotFittedError
 
 from pyrdf2vec.graphs import RDFLoader
 from pyrdf2vec.rdf2vec import RDF2VecTransformer
-from pyrdf2vec.samplers import UniformSampler
-from pyrdf2vec.walkers import RandomWalker
 
 np.random.seed(42)
 random.seed(42)
@@ -31,14 +29,9 @@ WALKS: DefaultDict[rdflib.URIRef, rdflib.URIRef] = defaultdict(list)
 
 class TestRDF2VecTransformer:
     def test_fit(self):
-        transformer = RDF2VecTransformer(
-            500, [RandomWalker(2, 5, UniformSampler(inverse=False))]
-        )
-
-        # The provided entities to fit() should be in the KG
+        transformer = RDF2VecTransformer()
         with pytest.raises(ValueError):
-            non_existing_entities = ["does", "not", "exist"]
-            transformer.fit(KG, non_existing_entities)
+            transformer.fit(KG, ["does", "not", "exist"])
         transformer.fit(KG, ENTITIES_SUBSET)
 
     def test_fit_transform(self):
@@ -53,5 +46,5 @@ class TestRDF2VecTransformer:
         with pytest.raises(NotFittedError):
             transformer.transform(ENTITIES_SUBSET)
         transformer.fit(KG, ENTITIES_SUBSET)
-        features_vectors = transformer.transform(ENTITIES_SUBSET)
-        assert type(features_vectors) == list
+        features = transformer.transform(ENTITIES_SUBSET)
+        assert type(features) == list
