e4c1f9d70029025e1e3482d271adfea90a701c0c
Terencio Agozzino <terencio.agozzino@gmail.com>
2020-10-27 15:02:53 +0100

    style: better test_graph

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -31,7 +31,6 @@ class TestVertex:
         assert a != b
 
 
-# Creating a small, artificial graph in rdflib.Graph
 # Alice -(knows)-> Bob -(knows)-> Casper
 #       -(knows)-> Dean
 g = rdflib.Graph()
@@ -56,58 +55,37 @@ g.add(
         rdflib.URIRef("http://pyRDF2Vec#Casper"),
     )
 )
-
-# Serialize the graph
 g.serialize("tmp.ttl", format="turtle")
 
 # Host a local endpoint
-old_stdout = sys.stdout
-old_stderr = sys.stderr
-sys.stdout = open(os.devnull, "w")
-sys.stderr = open(os.devnull, "w")
+old_stderr, old_stdout = sys.stderr, sys.stdout
+sys.stderr, sys.stdout = open(os.devnull, "w"), open(os.devnull, "w")
 proc = multiprocessing.Process(target=serve, daemon=True, args=(g,))
 proc.start()
 time.sleep(3)
 sys.stdout = old_stdout
 sys.stderr = old_stderr
 
-LOCAL_KNOWLEDGE_GRAPH = KG(location="tmp.ttl", file_type="turtle")
-
-REMOTE_KNOWLEDGE_GRAPH = KG(
-    location="http://localhost:5000/sparql", is_remote=True
-)
+LOCAL_KG = KG(location="tmp.ttl", file_type="turtle")
+REMOTE_KG = KG(location="http://localhost:5000/sparql", is_remote=True)
 
 
 class TestKG:
-    # def test_visualise(self):
-    #     KNOWLEDGE_GRAPH.visualise()
-    #     assert True
-
-    # def test_add_edge(self):
-    #     KNOWLEDGE_GRAPH.add_edge(a, c)
-    #     assert True
-
     def test_get_neighbors(self):
-        for graph in [LOCAL_KNOWLEDGE_GRAPH, REMOTE_KNOWLEDGE_GRAPH]:
+        for graph in [LOCAL_KG, REMOTE_KG]:
             neighbors = graph.get_hops("http://pyRDF2Vec#Alice")
-            predicates = [x[0] for x in neighbors]
 
-            assert {str(x) for x in predicates} == {"http://pyRDF2Vec#knows"}
+            predicates = [x[0] for x in neighbors]
+            assert {str(predicate) for predicate in predicates} == {
+                "http://pyRDF2Vec#knows"
+            }
 
             objects = [x[1] for x in neighbors]
             assert Vertex("http://pyRDF2Vec#Bob") in objects
             assert Vertex("http://pyRDF2Vec#Dean") in objects
 
-    # def test_inv_get_neighbors(self):
-    #     KNOWLEDGE_GRAPH.get_inv_neighbors(a)
-    #     assert True
 
-    # def test_remove_edge(self):
-    #     KNOWLEDGE_GRAPH.remove_edge(a, c)
-    #     assert True
-
-
-# Closing the server and removing the temporary rdf file
+# Closing the server and removing the temporary RDF file
 proc.terminate()
 proc.join()
 os.remove("tmp.ttl")
