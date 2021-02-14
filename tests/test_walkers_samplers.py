21e3ae738ee8cc220c87a423f2ab9cbeeb5daed8
Terencio Agozzino <terencio.agozzino@gmail.com>
2021-02-14 17:39:00 +0100

    fix: test_walkers_samplers

--- tests/test_walkers_samplers.py
+++ tests/test_walkers_samplers.py
@@ -3,7 +3,6 @@ import itertools
 
 import pandas as pd
 import pytest
-import rdflib
 
 from pyrdf2vec.graphs import KG
 from pyrdf2vec.rdf2vec import RDF2VecTransformer
@@ -25,16 +24,12 @@ from pyrdf2vec.samplers import (  # isort: skip
     UniformSampler,
 )
 
-KNOWLEDGE_GRAPH = KG(
-    "samples/mutag/mutag.owl",
-    skip_predicates={"http://dl-learner.org/carcinogenesis#isMutagenic"},
-)
-
 ENTITIES = [
-    rdflib.URIRef(x)
-    for x in pd.read_csv("samples/mutag/train.tsv", sep="\t", header=0)["bond"]
+    entity
+    for entity in pd.read_csv("samples/mutag/train.tsv", sep="\t", header=0)[
+        "bond"
+    ]
 ]
-ENTITIES_SUBSET = ENTITIES[:5]
 
 
 SAMPLER_CLASSES = {
@@ -78,4 +73,12 @@ class TestRDF2Vec:
     def test_fit_transform(self, walker, sampler):
         assert RDF2VecTransformer(
             walkers=[walker(2, 5, sampler(), n_jobs=-1, random_state=42)]
-        ).fit_transform(KNOWLEDGE_GRAPH, ENTITIES_SUBSET)
+        ).fit_transform(
+            KG(
+                "samples/mutag/mutag.owl",
+                skip_predicates={
+                    "http://dl-learner.org/carcinogenesis#isMutagenic"
+                },
+            ),
+            ENTITIES[:5],
+        )
