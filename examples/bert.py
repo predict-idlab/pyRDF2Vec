170f8285ea85f50c156359f5b2cd1fadc29c8503
Terencio Agozzino <terencio.agozzino@gmail.com>
2021-02-17 16:55:59 +0100

    feature: add beginning with BERT

---
+++ examples/bert.py
@@ -0,0 +1,119 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.manifold import TSNE
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.svm import SVC
+
+from pyrdf2vec import RDF2VecTransformer
+from pyrdf2vec.embedders import BERT
+from pyrdf2vec.graphs import KG
+from pyrdf2vec.walkers import RandomWalker
+
+# Ensure the determinism of this script by initializing a pseudo-random number.
+RANDOM_STATE = 42
+
+test_data = pd.read_csv("samples/mutag/test.tsv", sep="\t")
+train_data = pd.read_csv("samples/mutag/train.tsv", sep="\t")
+
+train_entities = [entity for entity in train_data["bond"]]
+train_labels = list(train_data["label_mutagenic"])
+
+test_entities = [entity for entity in test_data["bond"]]
+test_labels = list(test_data["label_mutagenic"])
+
+entities = train_entities + test_entities
+labels = train_labels + test_labels
+
+embeddings = RDF2VecTransformer(
+    # Ensure random determinism for Word2Vec.
+    # Must be used with PYTHONHASHSEED.
+    BERT(seed=RANDOM_STATE),
+    # Extract all walks of depth 2 for each entity using two processes
+    # and use a random state to ensure that the same walks are generated for
+    # the entities.
+    walkers=[RandomWalker(2, None, n_jobs=2, random_state=RANDOM_STATE)],
+    verbose=1,
+).fit_transform(
+    KG(
+        "samples/mutag/mutag.owl",
+        skip_predicates={"http://dl-learner.org/carcinogenesis#isMutagenic"},
+    ),
+    entities,
+)
+
+print(embeddings)
+
+train_embeddings = embeddings[: len(train_entities)]
+test_embeddings = embeddings[len(train_entities) :]
+
+# Fit a Support Vector Machine on train embeddings.
+clf = SVC(random_state=RANDOM_STATE)
+clf.fit(train_embeddings, train_labels)
+
+# Evaluate the Support Vector Machine on test embeddings.
+predictions = clf.predict(test_embeddings)
+print(f"Accuracy: {accuracy_score(test_labels, predictions) * 100 :.4f}%")
+print(confusion_matrix(test_labels, predictions))
+
+# Reduce the dimensions of entity embeddings to represent them in a 2D plane.
+X_tsne = TSNE(random_state=RANDOM_STATE).fit_transform(
+    train_embeddings + test_embeddings
+)
+
+# Define the color map
+colors = ["r", "g"]
+color_map = {}
+for i, label in enumerate(set(labels)):
+    color_map[label] = colors[i]
+
+# Set the graph with a certain size.
+plt.figure(figsize=(10, 4))
+
+# Plot the train embeddings
+plt.scatter(
+    X_tsne[: len(train_entities), 0],
+    X_tsne[: len(train_entities), 1],
+    edgecolors=[color_map[i] for i in labels[: len(train_entities)]],
+    facecolors=[color_map[i] for i in labels[: len(train_entities)]],
+)
+
+# Plot the test embeddings.
+plt.scatter(
+    X_tsne[len(train_entities) :, 0],
+    X_tsne[len(train_entities) :, 1],
+    edgecolors=[color_map[i] for i in labels[len(train_entities) :]],
+    facecolors="none",
+)
+
+# Annotate few points.
+plt.annotate(
+    entities[25].split("/")[-1],
+    xy=(X_tsne[25, 0], X_tsne[25, 1]),
+    xycoords="data",
+    xytext=(0.01, 0.0),
+    fontsize=8,
+    textcoords="axes fraction",
+    arrowprops=dict(arrowstyle="->", facecolor="black"),
+)
+plt.annotate(
+    entities[35].split("/")[-1],
+    xy=(X_tsne[35, 0], X_tsne[35, 1]),
+    xycoords="data",
+    xytext=(0.4, 0.0),
+    fontsize=8,
+    textcoords="axes fraction",
+    arrowprops=dict(arrowstyle="->", facecolor="black"),
+)
+
+# Create a legend
+plt.scatter([], [], edgecolors="r", facecolors="r", label="train -")
+plt.scatter([], [], edgecolors="g", facecolors="g", label="train +")
+plt.scatter([], [], edgecolors="r", facecolors="none", label="test -")
+plt.scatter([], [], edgecolors="g", facecolors="none", label="test +")
+plt.legend(loc="upper right", ncol=2)
+
+# Display the graph with a title, removing the axes for
+# better readability.
+plt.title("pyRDF2Vec", fontsize=32)
+plt.axis("off")
+plt.show()
---
+++ pyrdf2vec/embedders/bert.py
@@ -0,0 +1,111 @@
+from typing import List
+
+import numpy as np
+import transformers
+from sklearn.utils.validation import check_is_fitted
+from torch.utils.data import Dataset
+from transformers import (BertConfig, BertForMaskedLM, BertTokenizer,
+                          DataCollatorForLanguageModeling, DistilBertConfig,
+                          DistilBertForMaskedLM, DistilBertTokenizer, Trainer,
+                          TrainingArguments)
+
+from pyrdf2vec.embedders import Embedder
+
+
+class WalkDataset(Dataset):
+    def __init__(self, corpus, tokenizer):
+        self.walks = [
+            tokenizer(
+                " ".join(walk), padding=True, truncation=True, max_length=512
+            )
+            for walk in corpus
+        ]
+
+    def __len__(self):
+        return len(self.walks)
+
+    def __getitem__(self, i):
+        return self.walks[i]
+
+
+class BERT(Embedder):
+    """Defines BERT embedding technique."""
+
+    def __init__(self, vocab_filename="bert-kg", seed=None):
+        self.seed = seed
+        self.vocab_filename = vocab_filename
+        self.verbose = True
+        self._vocabulary_size = 0
+
+    def _build_vocabulary(
+        self, hops: List[str], is_update: bool = False
+    ) -> None:
+        with open(self.vocab_filename, "w") as f:
+            if not is_update:
+                for token in ["[PAD]", "[UNK]", "[CLS]", "[SEP]", "[MASK]"]:
+                    f.write("%s\n" % token)
+                    self._vocabulary_size += 1
+            for hop in hops:
+                f.write("%s\n" % hop)
+                self._vocabulary_size += 1
+
+    def fit(self, corpus, is_update=False):
+        unique_hops = list({hop for walk in corpus for hop in walk})
+        self._build_vocabulary(unique_hops, is_update)
+
+        # tokenizer = BertTokenizer(
+        #     vocab_file=self.vocab_filename,
+        #     do_lower_case=False,
+        #     never_split=unique_hops,
+        # )
+
+        self.tokenizer = DistilBertTokenizer(
+            vocab_file=self.vocab_filename,
+            do_lower_case=False,
+            never_split=unique_hops,
+        )
+
+        # self.model_ = BertForMaskedLM(
+        #     BertConfig(
+        #         vocab_size=self._vocabulary_size,
+        #         max_position_embeddings=512,
+        #         type_vocab_size=1,
+        #     )
+        # )
+
+        self.model_ = DistilBertForMaskedLM(
+            DistilBertConfig(
+                vocab_size=self._vocabulary_size,
+                max_position_embeddings=512,
+                type_vocab_size=1,
+            )
+        )
+
+        Trainer(
+            model=self.model_,
+            args=TrainingArguments(
+                output_dir="./bert",
+                overwrite_output_dir=True,
+                num_train_epochs=1,
+                warmup_steps=500,
+                weight_decay=0.2,
+                logging_dir="./logs",
+                seed=self.seed,
+                dataloader_num_workers=2,
+                prediction_loss_only=True,
+            ),
+            data_collator=DataCollatorForLanguageModeling(
+                tokenizer=self.tokenizer
+            ),
+            train_dataset=WalkDataset(corpus, self.tokenizer),
+        ).train()
+        return self
+
+    def transform(self, entities: List[str]):
+        check_is_fitted(self, ["model_"])
+        return [
+            self.model_.distilbert.embeddings.word_embeddings.weight[
+                self.tokenizer.init_kwargs["never_split"].index(entity)
+            ]
+            for entity in entities
+        ]
