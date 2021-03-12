20b6249a07e436dcac27c59598eb832270a550c1
Terencio Agozzino <terencio.agozzino@gmail.com>
2021-03-12 14:49:42 +0100

    chore: rename online-training-mutag

--- examples/online-training.py
+++ examples/online-training.py
@@ -1,74 +1,169 @@
 import os
+import random
 
-import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.manifold import TSNE
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
 
 from pyrdf2vec import RDF2VecTransformer
 from pyrdf2vec.embedders import Word2Vec
 from pyrdf2vec.graphs import KG
+from pyrdf2vec.samplers import PageRankSampler
 from pyrdf2vec.walkers import RandomWalker
 
 # Ensure the determinism of this script by initializing a pseudo-random number.
 RANDOM_STATE = 42
+random.seed(RANDOM_STATE)
 
-data = pd.read_csv("samples/countries-cities/entities.tsv", sep="\t")
+test_data = pd.read_csv("samples/mutag/test.tsv", sep="\t")
+train_data = pd.read_csv("samples/mutag/train.tsv", sep="\t")
 
-# Defined that the Knowledge Graph (KG) is remotely located, as well as a set
-# of predicates to exclude from this KG.
+train_entities = [entity for entity in train_data["bond"]]
+train_labels = list(train_data["label_mutagenic"])
+
+test_entities = [entity for entity in test_data["bond"]]
+test_labels = list(test_data["label_mutagenic"])
+
+entities = train_entities + test_entities
+labels = train_labels + test_labels
+
+# Defines the MUTAG KG with the predicates to be skipped and the paths of
+# the literals to be extracted.
 kg = KG(
-    "https://dbpedia.org/sparql",
-    skip_predicates={"www.w3.org/1999/02/22-rdf-syntax-ns#type"},
+    "samples/mutag/mutag.owl",
+    skip_predicates={"http://dl-learner.org/carcinogenesis#isMutagenic"},
+    literals=[
+        [
+            "http://dl-learner.org/carcinogenesis#hasBond",
+            "http://dl-learner.org/carcinogenesis#inBond",
+        ],
+        [
+            "http://dl-learner.org/carcinogenesis#hasAtom",
+            "http://dl-learner.org/carcinogenesis#charge",
+        ],
+    ],
 )
 
-# Train and save the Word2Vec model according to the KG, the entities, a
-# walking strategy, and use a random state to ensure to generate the same walks
-# for entities.
 transformer = RDF2VecTransformer(
     # Ensure random determinism for Word2Vec.
     # Must be used with PYTHONHASHSEED.
     Word2Vec(workers=1),
-    # Extract a maximum of 25 walks of depth 4 for each entity, use a random
-    # state to ensure that the same walks are generated for the entities.
-    walkers=[RandomWalker(4, 25, random_state=RANDOM_STATE)],
+    # Extract all walks of depth 2 for each entity by using the PageRank
+    # sampling strategy with one process and a random state to ensure that the
+    # same walks are generated for the entities.
+    walkers=[
+        RandomWalker(
+            2,
+            50,
+            sampler=PageRankSampler(),
+            n_jobs=1,
+            random_state=RANDOM_STATE,
+        )
+    ],
     verbose=1,
 )
-transformer.fit_transform(
-    kg,
-    [entity for entity in data["location"]],
-)
-transformer.save("countries")
+embeddings, literals = transformer.fit_transform(kg, entities)
+transformer.save("mutag")
 
-# Add new entities in the existing KG and update the previously trained model
-# with the embeddings of these new entities.
-data = pd.DataFrame(
-    {
-        "location": "http://dbpedia.org/resource/Afghanistan",
-        "id": 23.0,
-        "label": 0.0,
-    },
-    index=[0],
+train_embeddings = embeddings[: len(train_entities)]
+test_embeddings = embeddings[len(train_entities) :]
+
+# Fit a Support Vector Machine on train embeddings and pick the best
+# C-parameters (regularization strength).
+clf = GridSearchCV(
+    SVC(random_state=RANDOM_STATE), {"C": [10 ** i for i in range(-3, 4)]}
 )
-transformer = RDF2VecTransformer(verbose=1).load("countries")
-embeddings = transformer.fit_transform(
+clf.fit(train_embeddings, train_labels)
+
+# Evaluate the Support Vector Machine on test embeddings.
+predictions = clf.predict(test_embeddings)
+print(
+    f"Predicted {len(test_entities)} entities with an accuracy of "
+    + f"{accuracy_score(test_labels, predictions) * 100 :.4f}%"
+)
+print(f"Confusion Matrix ([[TN, FP], [FN, TP]]):")
+print(confusion_matrix(test_labels, predictions))
+
+print("\nAdding 20 mores entities.")
+
+new_data = pd.read_csv("samples/mutag/online-training.tsv", sep="\t")
+new_entities = [entity for entity in new_data["bond"]]
+new_labels = list(new_data["label_mutagenic"])
+
+transformer = RDF2VecTransformer(
+    Word2Vec(workers=1),
+    walkers=[
+        RandomWalker(
+            2,
+            50,
+            sampler=PageRankSampler(),
+            n_jobs=1,
+            random_state=RANDOM_STATE,
+        ),
+    ],
+    verbose=1,
+).load("mutag")
+embeddings, literals = transformer.fit_transform(
     kg,
-    [entity for entity in data["location"]],
+    new_entities,
     is_update=True,
 )
 
-# Reduce the dimensions of entity embeddings to represent them in a 2D plane.
-X_tsne = TSNE(random_state=RANDOM_STATE).fit_transform(embeddings)
+train_embeddings = embeddings[: len(train_entities)]
+new_embeddings = embeddings[-len(new_entities) :]
+test_embeddings = embeddings[len(train_entities) :][: -len(new_entities)]
 
-# Plot the embeddings of entities in a 2D plane, annotating them.
-plt.figure(figsize=(10, 4))
-plt.scatter(X_tsne[:, 0], X_tsne[:, 1])
-for x, y, t in zip(X_tsne[:, 0], X_tsne[:, 1], transformer._entities):
-    plt.annotate(t.split("/")[-1], (x, y))
+# Fit a Support Vector Machine on train embeddings and pick the best
+# C-parameters (regularization strength).
+clf = GridSearchCV(
+    SVC(random_state=RANDOM_STATE), {"C": [10 ** i for i in range(-3, 4)]}
+)
+clf.fit(train_embeddings + new_embeddings, train_labels + new_labels)
 
-# Display the graph with a title, removing the axes for better readability.
-plt.title("Countries (Online Training)", fontsize=16)
-plt.axis("off")
-plt.show()
+# Evaluate the Support Vector Machine on test embeddings.
+predictions = clf.predict(test_embeddings)
+print(
+    f"Predicted {len(test_entities)} entities with an accuracy of "
+    + f"{accuracy_score(test_labels, predictions) * 100 :.4f}%"
+)
+print(f"Confusion Matrix ([[TN, FP], [FN, TP]]):")
+print(confusion_matrix(test_labels, predictions))
 
-# Remove the binary file related to the RDF2VecTransformer.
-os.remove("countries")
+transformer = RDF2VecTransformer(
+    Word2Vec(workers=1),
+    walkers=[
+        RandomWalker(
+            2,
+            50,
+            sampler=PageRankSampler(),
+            n_jobs=1,
+            random_state=RANDOM_STATE,
+        ),
+    ],
+    verbose=1,
+)
+embedding, literals = transformer.fit_transform(
+    kg, train_entities + new_entities + test_entities
+)
+
+train_embeddings = embeddings[: len(train_entities) + len(new_entities)]
+test_embeddings = embeddings[len(train_entities) + len(new_entities) :]
+
+# Fit a Support Vector Machine on train embeddings and pick the best
+# C-parameters (regularization strength).
+clf = GridSearchCV(
+    SVC(random_state=RANDOM_STATE), {"C": [10 ** i for i in range(-3, 4)]}
+)
+clf.fit(train_embeddings, train_labels + new_labels)
+
+# Evaluate the Support Vector Machine on test embeddings.
+predictions = clf.predict(test_embeddings)
+print(
+    f"Predicted {len(test_entities)} entities with an accuracy of "
+    + f"{accuracy_score(test_labels, predictions) * 100 :.4f}%"
+)
+print(f"Confusion Matrix ([[TN, FP], [FN, TP]]):")
+print(confusion_matrix(test_labels, predictions))
+
+os.remove("mutag")
