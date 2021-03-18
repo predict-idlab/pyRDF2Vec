d3ecd632acd0281ad31ed903a86d5bc247896e52
Terencio Agozzino <terencio.agozzino@gmail.com>
2021-03-18 09:42:21 +0100

    chore: update tests with KG

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pytest
 
 from pyrdf2vec.graphs import KG, Vertex
@@ -37,7 +38,7 @@ GRAPH = [
 ]
 URL = "http://pyRDF2Vec"
 
-LOCAL_KG = KG(cache=None)
+LOCAL_KG = KG(cache=None, literals=[[f"{URL}#knows"]])
 
 
 class TestKG:
@@ -51,13 +52,90 @@ class TestKG:
             )
             LOCAL_KG.add_walk(subj, pred, obj)
 
+    def test_add_walk(self):
+        kg = KG(skip_predicates={f"{URL}#predicate"})
+        for row in GRAPH:
+            subj = Vertex(f"{URL}#{row[0]}")
+            obj = Vertex((f"{URL}#{row[2]}"))
+            pred = Vertex(
+                (f"{URL}#{row[1]}"), predicate=True, vprev=subj, vnext=obj
+            )
+            assert kg.add_walk(subj, pred, obj) is True
+        subj = Vertex(f"{URL}#{GRAPH[0][0]}")
+        obj = Vertex(f"{URL}#{GRAPH[0][2]}")
+        pred = Vertex(f"{URL}#predicate")
+        assert kg.add_walk(subj, pred, obj) is False
+
+    def test_cast_literals(self, setup):
+        literals = LOCAL_KG._cast_literals([[]])
+        assert len(literals) == 1
+        assert np.NaN in literals
+
+        literals = LOCAL_KG._cast_literals([[], []])
+        assert len(literals) == 2
+        assert literals == [np.nan, np.nan]
+
+        literals = LOCAL_KG._cast_literals([[f"{URL}#Bob"]])
+        assert len(literals) == 1
+        assert f"{URL}#Bob" in literals
+
+        literals = LOCAL_KG._cast_literals([[f"{URL}#Bob", f"{URL}#Dean"]])
+        assert len(literals) == 1
+        print(literals)
+        assert (f"{URL}#Bob", f"{URL}#Dean") in literals
+
+        literals = LOCAL_KG._cast_literals([["0.42"]])
+        assert len(literals) == 1
+        assert 0.42 in literals
+
+        literals = LOCAL_KG._cast_literals([["0.42", "0.37"]])
+        assert len(literals) == 1
+        assert (0.42, 0.37) in literals
+
+    def test_get_pliterals(self, setup):
+        alice_literals = LOCAL_KG.get_pliterals(
+            f"{URL}#Alice", LOCAL_KG.literals[0]
+        )
+        assert isinstance(alice_literals, list)
+        assert len(alice_literals) == 2
+        assert f"{URL}#Bob" in alice_literals
+        assert f"{URL}#Dean" in alice_literals
+
+        bob_literals = LOCAL_KG.get_pliterals(
+            f"{URL}#Bob", LOCAL_KG.literals[0]
+        )
+        assert isinstance(bob_literals, list)
+        assert len(bob_literals) == 1
+        assert f"{URL}#Casper" in bob_literals
+
+        dean_literals = LOCAL_KG.get_pliterals(
+            f"{URL}#Dean", LOCAL_KG.literals[0]
+        )
+        assert isinstance(dean_literals, list)
+        assert len(dean_literals) == 0
+        assert dean_literals == []
+
     def test_get_hops(self, setup):
         neighbors = LOCAL_KG.get_hops(Vertex(f"{URL}#Alice"))
         predicates = [neighbor[0] for neighbor in neighbors]
         objects = [neighbor[1] for neighbor in neighbors]
+
+        assert len(neighbors) == 2
+        assert len(predicates) == 2
+        assert len(objects) == 2
+
         assert {predicate.name for predicate in predicates} == {f"{URL}#knows"}
         assert Vertex(f"{URL}#Bob") in objects
         assert Vertex(f"{URL}#Dean") in objects
+
+        neighbors = LOCAL_KG.get_hops(Vertex(f"{URL}#Bob"), is_reverse=True)
+        predicates = [neighbor[0] for neighbor in neighbors]
+        objects = [neighbor[1] for neighbor in neighbors]
+
+        assert len(neighbors) == 1
+        assert len(predicates) == 1
+        assert len(objects) == 1
+        assert Vertex(f"{URL}#Alice") in objects
 
     def test_get_neighbors(self, setup):
         alice_predicates = [
@@ -122,6 +200,11 @@ class TestKG:
             == 0
         )
 
+    def test_init_local_kg(self):
+        kg = KG("samples/mutag/mutag.owl")
+        assert len(kg._transition_matrix) > 0
+        assert len(kg._inv_transition_matrix) > 0
+
     def test_invalid_file(self):
         with pytest.raises(FileNotFoundError):
             KG("foo")
@@ -145,6 +228,11 @@ class TestKG:
             for vertex in hops
             if vertex.predicate is True
         ]
+
+        with pytest.raises(ValueError):
+            LOCAL_KG._is_remote = True
+            LOCAL_KG.remove_edge(vtx_alice, predicates[0])
+        LOCAL_KG._is_remote = False
 
         assert LOCAL_KG.remove_edge(vtx_alice, predicates[0]) is True
         assert len(LOCAL_KG.get_hops(vtx_alice)) == 1
