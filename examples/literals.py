0e70cff11cc3e09ac55217ed846f5cbe9b41e639
Terencio Agozzino <terencio.agozzino@gmail.com>
2021-03-31 16:09:56 +0200

    style: change default epochs in examples

--- examples/literals.py
+++ examples/literals.py
@@ -29,7 +29,7 @@ labels = train_labels + test_labels
 embeddings, literals = RDF2VecTransformer(
     # Ensure random determinism for Word2Vec.
     # Must be used with PYTHONHASHSEED.
-    Word2Vec(workers=1),
+    Word2Vec(workers=1, epochs=10),
     # Extract all walks with a maximum depth of 2 for each entity using two
     # processes and use a random state to ensure that the same walks are
     # generated for the entities.
