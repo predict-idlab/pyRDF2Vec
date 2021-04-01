0a00bdc22c2e753aafb5f45d3abb75e140eba430
Terencio Agozzino <terencio.agozzino@gmail.com>
2021-04-01 16:26:13 +0200

    chore: fix computing_ngrams function for FastText

--- pyrdf2vec/embedders/fasttext.py
+++ pyrdf2vec/embedders/fasttext.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import re
-from typing import Any, Dict, List
+from typing import Any, List
 
 import attr
 import numpy as np
@@ -38,10 +38,16 @@ class FastText(Embedder):
         kwargs: The keyword arguments dictionary.
             Defaults to { bucket=2000000, min_count=0, max_n=0, min_n=0,
                 negative=20, vector_size=500 }
+        func_computing_ngrams: The function to call for the computation of
+            ngrams. In case of reimplementation, it is important to respect the
+            signature imposed by gensim:
+            func(entity: str, minn: int = 0, maxn: int = 0) -> List[bytes]
+            Defaults to compute_ngrams_bytes
 
     """
 
     kwargs = attr.ib(init=False, default=None)
+    func_computing_ngrams = attr.ib(kw_only=True, repr=False)
     _model = attr.ib(init=False, type=FT, default=None, repr=False)
 
     def __init__(self, **kwargs):
@@ -52,54 +58,20 @@ class FastText(Embedder):
             "vector_size": 500,
             **kwargs,
         }
+        if "func_computing_ngrams" in self.kwargs:
+            self.func_computing_ngrams = self.kwargs["func_computing_ngrams"]
+            self.kwargs.pop("func_computing_ngrams")
+        else:
+            self.func_computing_ngrams = None
+
         self._model = FT(**self.kwargs)
         self._model.wv = RDFFastTextKeyedVectors(
             vector_size=self.kwargs["vector_size"],
             bucket=self.kwargs["bucket"],
+            func_computing_ngrams=self.func_computing_ngrams,
         )
         self._model.wv.vectors_vocab_lockf = ones(1, dtype=REAL)
         self._model.wv.vectors_ngrams_lockf = ones(1, dtype=REAL)
-
-    def estimate_memory(
-        self, vocab_size: int = None, report: Dict[Any, Any] = None
-    ) -> Dict[Any, Any]:
-        """Reimplementation of the estimate_memory method of gensim. This
-        overwrite is needed to call our ft_ngram_hashes method.
-
-        Args:
-            vocab_size: The size of the vocabulary
-            report: The existing memory report.
-
-        Returns:
-            The memory report.
-
-        """
-        vocab_size = vocab_size or len(self._model.wv)
-        vec_size = self._model.vector_size * np.dtype(np.float32).itemsize
-        l1_size = self._model.layer1_size * np.dtype(np.float32).itemsize
-        report = report or {}
-        report["vocab"] = len(self._model.wv) * (
-            700 if self._model.hs else 500
-        )
-        report["syn0_vocab"] = len(self._model.wv) * vec_size
-        if self._model.hs:
-            report["syn1"] = len(self._model.wv) * l1_size
-        if self._model.negative:
-            report["syn1neg"] = len(self._model.wv) * l1_size
-        if self._model.wv.bucket:
-            report["syn0_ngrams"] = self._model.wv.bucket * vec_size
-            num_ngrams = 0
-            for word in self._model.wv.key_to_index:
-                hashes = ft_ngram_hashes(word, 0, 0, self._model.wv.bucket)
-                num_ngrams += len(hashes)
-            # A list (64 bytes) with one np.array (100 bytes) per key, with a
-            # total of num_ngrams uint32s (4 bytes) amongst them.  Only used
-            # during training, not stored with the model.
-            report["buckets_word"] = (
-                64 + (100 * len(self._model.wv)) + (4 * num_ngrams)
-            )
-        report["total"] = sum(report.values())
-        return report
 
     def fit(
         self, walks: List[List[SWalk]], is_update: bool = False
@@ -150,9 +122,12 @@ class RDFFastTextKeyedVectors(FastTextKeyedVectors):
 
     bucket: int = attr.ib(default=2000000)
     vector_size: int = attr.ib(default=500)
+    func_computing_ngrams = attr.ib(kw_only=True, default=None, repr=False)
 
     def __attrs_post_init__(self):
         super().__init__(self.vector_size, 0, 0, self.bucket)
+        if self.func_computing_ngrams is None:
+            self.func_computing_ngrams = self.compute_ngrams_bytes
 
     def get_vector(self, word, norm=False):
         if word in self.key_to_index:
@@ -164,7 +139,7 @@ class RDFFastTextKeyedVectors(FastTextKeyedVectors):
         else:
             word_vec = np.zeros(self.vectors_ngrams.shape[1], dtype=np.float32)
             ngram_weights = self.vectors_ngrams
-            ngram_hashes = ft_ngram_hashes(word, 0, 0, self.bucket)
+            ngram_hashes = self.ft_ngram_hashes(word, 0, 0, self.bucket)
             if len(ngram_hashes) == 0:
                 #
                 # If it is impossible to extract _any_ ngrams from the input
@@ -197,57 +172,60 @@ class RDFFastTextKeyedVectors(FastTextKeyedVectors):
 
         for i, word in enumerate(self.index_to_key):
             self.buckets_word[i] = np.array(
-                ft_ngram_hashes(word, 0, 0, self.bucket),
+                self.ft_ngram_hashes(word, 0, 0, self.bucket),
                 dtype=np.uint32,
             )
 
+    def compute_ngrams_bytes(
+        self, entity: str, minn: int = 0, maxn: int = 0
+    ) -> List[bytes]:
+        """Reimplementation of the compute_ngrams_bytes method of gensim. This
+           overwrite is needed to call our compute_ngrams_bytes method.
 
-def compute_ngrams_bytes(
-    entity: str, minn: int = 0, maxn: int = 0
-) -> List[bytes]:
-    """Reimplementation of the ft_ngram_hahes method of gensim. This overwrite
-    is needed to call our compute_ngrams_bytes method.
+        Args:
+            entity: The entity to hash the ngrams.
+            minn: Minimum length of char n-grams to be used for training entity
+                representations.
+                Defaults to 0.
+            maxn: Maximum length of char n-grams to be used for training
+                entity representations.
+                Defaults to 0.
 
-    Args:
-        entity: The entity to hash the ngrams.
-        minn: Minimum length of char n-grams to be used for training entity
-            representations.
-            Defaults to 0.
-        maxn: Maximum length of char n-grams to be used for training entity
-            representations.
-            Defaults to 0.
+        Returns:
+            The ngrams bytes.
 
-    Returns:
-        The ngrams bytes.
+        """
+        if "http" in entity:
+            ngrams = " ".join(re.split("[#]", entity)).split()
+            return [str.encode(ngram) for ngram in ngrams]
+        return [str.encode(entity)]
 
-    """
-    if "http" in entity:
-        ngrams = " ".join(re.split("[#]", entity)).split()
-        return [str.encode(ngram) for ngram in ngrams]
-    return [str.encode(entity)]
+    def ft_ngram_hashes(
+        self,
+        entity: str,
+        minn: int = 0,
+        maxn: int = 0,
+        num_buckets: int = 2000000,
+    ) -> List[Any]:
+        """Reimplementation of the ft_ngram_hahes method of gensim. This
+        overwrite is needed to call our compute_ngrams_bytes method.
 
+        Args:
+             entity: The entity to hash the ngrams.
+             minn: Minimum length of char n-grams to be used for training
+                entity representations.
+                Defaults to 0.
+             maxn: Maximum length of char n-grams to be used for training
+                entity representations.
+                Defaults to 0.
+             num_buckets: Character ngrams are hashed into a fixed number of
+                buckets, in order to limit the memory usage of the model.
+                Defaults to 2000000.
 
-def ft_ngram_hashes(
-    entity: str, minn: int = 0, maxn: int = 0, num_buckets: int = 2000000
-) -> List[Any]:
-    """Reimplementation of the ft_ngram_hahes method of gensim. This overwrite
-    is needed to call our compute_ngrams_bytes method.
+         Returns:
+             The ngrams hashes.
 
-    Args:
-        entity: The entity to hash the ngrams.
-        minn: Minimum length of char n-grams to be used for training entity
-            representations.
-            Defaults to 0.
-        maxn: Maximum length of char n-grams to be used for training entity
-            representations.
-            Defaults to 0.
-        num_buckets: Character ngrams are hashed into a fixed number of
-            buckets, in order to limit the memory usage of the model.
-            Defaults to 2000000.
-    Returns:
-        The ngrams hashes.
-
-    """
-    encoded_ngrams = compute_ngrams_bytes(entity, minn, maxn)
-    hashes = [ft_hash_bytes(n) % num_buckets for n in encoded_ngrams]
-    return hashes
+        """
+        encoded_ngrams = self.func_computing_ngrams(entity, minn, maxn)
+        hashes = [ft_hash_bytes(n) % num_buckets for n in encoded_ngrams]
+        return hashes
