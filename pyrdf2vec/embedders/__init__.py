461a1daaf1689add7e31183995df0feb3f25a9bd
Terencio Agozzino <terencio.agozzino@gmail.com>
2021-04-27 15:04:48 +0200

    chore: add Doc2Vec in __init__

--- pyrdf2vec/embedders/__init__.py
+++ pyrdf2vec/embedders/__init__.py
@@ -1,7 +1,8 @@
 """isort:skip_file"""
 
 from .embedder import Embedder
+from .doc2vec import Doc2Vec
 from .fasttext import FastText
 from .word2vec import Word2Vec
 
-__all__ = ["Embedder", "FastText", "Word2Vec"]
+__all__ = ["Embedder", "Doc2Vec", "FastText", "Word2Vec"]
