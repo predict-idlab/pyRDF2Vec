9880c15917f4d7fc7d369b7df75a6bc1303df6c7
Terencio Agozzino <terencio.agozzino@gmail.com>
2021-04-27 15:04:04 +0200

    chore: add BERT in __init__

--- pyrdf2vec/embedders/__init__.py
+++ pyrdf2vec/embedders/__init__.py
@@ -1,7 +1,8 @@
 """isort:skip_file"""
 
 from .embedder import Embedder
+from .bert import BERT
 from .fasttext import FastText
 from .word2vec import Word2Vec
 
-__all__ = ["Embedder", "FastText", "Word2Vec"]
+__all__ = ["Embedder", "BERT", "FastText", "Word2Vec"]
